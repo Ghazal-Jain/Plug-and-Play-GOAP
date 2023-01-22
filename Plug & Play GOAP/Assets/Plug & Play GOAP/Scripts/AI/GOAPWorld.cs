07b5a4f231dcbe0b65ae3d73495bc693591bf80c
Ghazal J <ghazal.jain.97@gmail.com>
2023-01-22 15:27:04 -0800

    Demo Scene Setup

---
+++ Plug & Play GOAP/Assets/Plug & Play GOAP/Scripts/AI/GOAPWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Sealed Class:  a class that cannot be inherited by any class but can be instantiated*/
+
+public sealed class GOAPWorld 
+{
+    private static 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
